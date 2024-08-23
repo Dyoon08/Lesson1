ba542bf32052ced66fa8a439ddfbc7b9acc9c13f
Daniel Yoon <35455869-daniel1s1yoon@users.noreply.replit.com>
2024-08-23 21:29:20 +0000

    this code goes from lv 1 to lv 3

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+# age1 = int(input("What is your age"))
+# age2 = int(input("What is your age"))
+
+
+# if age1 > age2:
+#   print( "yay!" )
+# else:
+#   print("younger")  
+
+
+
+
+
+# num1 = int(input( "numorator" ))
+# num2 = int(input(" denominator" ))
+# if num1 % num2 == 0:
+#   print( "yes" )
+# elif num2 % num1 !=0:
+#   print( "no" )
+
+
+
+
+name="daniel"
+if "e" in name:
+  print( "yes" )
+else:
+  print ("no")
